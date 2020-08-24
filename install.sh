228204ebc952b75b1a71243a8fcf0cd87e495ff2
Jonathan Gould <jonathan.d.gould@gmail.com>
2020-08-24 12:16:18 -0400

    added vim plugin installation

--- install.sh
+++ install.sh
@@ -10,3 +10,11 @@ ln -sv ~/.dotfiles/profile ~/.profile
 ln -sv ~/.dotfiles/vimrc ~/.vimrc
 ln -sv ~/.dotfiles/zprofile ~/.zprofile
 ln -sv ~/.dotfiles/zshrc ~/.zshrc
+
+mkdir ~/.vim
+ln -sv ~/.dotfiles/vanilla.vim ~/.vim/vanilla.vim
+ln -sv ~/.dotfiles/plugin_stuff.vim ~/.vim/plugin_stuff.vim
+
+# install vim plugins
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
